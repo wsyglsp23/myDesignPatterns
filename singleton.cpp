e6111fdcb96bf3b4b990704afb13b0e2b53a2278
jianger <wsyglsp23@gmail.com>
2024-02-29 15:15:44 +0800

    add:singelton pattern

---
+++ singleton.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#define EHAN   0
+
+#if EHAN
+//饿汉式：线程安全，注意一定要在合适的地方去delete它
+class Singleton
+{
+public:
+    static Singleton* getInstance();
+private:
+    Singleton(){}                                    //构造函数私有
+    Singleton(const Singleton&) = delete;            //明确拒绝
+    Singleton& operator=(const Singleton&) = delete; //明确拒绝
+
+    static Singleton* m_pSingleton;
+};
+
+Singleton* Singleton::m_pSingleton = new Singleton();
+
+Singleton* Singleton::getInstance()
+{
+    return m_pSingleton;
+}
+#else
+/*
+    懒汉在获取实例的时候才会去new出对象
+*/
+#include <mutex>
+std::mutex mt;
+
+class Singleton
+{
+public:
+    static Singleton* getInstance();
+private:
+    Singleton(){}                                    //构造函数私有
+    Singleton(const Singleton&) = delete;            //明确拒绝
+    Singleton& operator=(const Singleton&) = delete; //明确拒绝
+
+    static Singleton* m_pSingleton;
+
+};
+Singleton* Singleton::m_pSingleton = NULL;
+
+Singleton* Singleton::getInstance()
+{
+    if(m_pSingleton == NULL)
+    {
+        
+        mt.lock();
+        if(m_pSingleton == NULL)
+        {
+            m_pSingleton = new Singleton();
+        }
+        mt.unlock();
+    }
+    return m_pSingleton;
+}
+
+
+#endif
+
+
+int main()
+{
+    auto singlev = Singleton::getInstance();
+    //auto singlev2 = *singlev; 赋值的操作是delete掉的
+    
+    return 0;
+}
